71de0181297df3d7424687328d14c21609cf8bea
davidajimati <ajimatidavid@gmail.com>
2023-07-25 13:12:49 +0100

    10-loops.js const used

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
-  let array1 = [];
+  const array1 = [];
   for (const idx of array) {
     array1[i] = appendString + idx;
     i += 1;

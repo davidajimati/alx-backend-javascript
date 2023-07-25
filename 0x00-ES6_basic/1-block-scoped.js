b61547f9d33dd0d78c245c149d8f7cc2aabe9b8d
davidajimati <ajimatidavid@gmail.com>
2023-07-25 09:42:06 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars
-    const task = true;
-    // eslint-disable-next-line no-unused-vars
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];

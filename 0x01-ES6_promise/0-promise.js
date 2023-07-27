73ca796f2c0502b5b150cedac6c6e2230335b1b8
davidajimati <ajimatidavid@gmail.com>
2023-07-27 10:10:53 +0100

    eslint conformations

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,13 @@
 export default function getResponseFromAPI() {
+  const a = 4;
+  const b = 3;
   return new Promise((resolve, reject) => {
-    resolve(setTimeout((() => {
-      resolve("Success");
-    }, 5000)))
-  })
+    if (a > b) {
+      resolve(setTimeout((() => {
+        resolve('Success');
+      }, 5000)));
+    } else {
+      reject(new Error());
+    }
+  });
 }

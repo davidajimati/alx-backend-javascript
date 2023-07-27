e23e6b02488fa282b0604171893c00bca1436079
davidajimati <ajimatidavid@gmail.com>
2023-07-27 12:35:38 +0100

    3-all.js edits

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
-      console.log(data[0].body, data[1].firstName, data[1].lastName);
+      console.log(data[1].body, data[0].firstName, data[0].lastName);
     })
     .catch(() => {
       console.log('Signup system offline');

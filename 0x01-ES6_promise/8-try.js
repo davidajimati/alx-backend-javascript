57fbaa1049501089a66db15a899dc79f6dcb1509
davidajimati <ajimatidavid@gmail.com>
2023-07-27 18:59:28 +0100

    eslint style checks

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   try {
-    if (denominator == 0) {
+    if (denominator === 0) {
       throw Error;
     } else {
       return numerator / denominator;

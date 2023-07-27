2c62c836dde3f9df110f140e047f532335205973
davidajimati <ajimatidavid@gmail.com>
2023-07-27 14:49:45 +0100

    es-lint

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   try {
     if (denominator == 0) {
-      throw Error
+      throw Error;
     } else {
       return numerator / denominator;
     }

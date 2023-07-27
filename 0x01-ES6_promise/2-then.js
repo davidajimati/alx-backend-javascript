0ea51f22b366c597cea0116fa2a8b9635a4b30f3
davidajimati <ajimatidavid@gmail.com>
2023-07-27 11:06:48 +0100

    2-then.js finally block implemented

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,16 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
-  });
+  try {
+    return new Promise((resolve, reject) => {
+      if (promise) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error());
+      }
+    });
+  } finally {
+    console.log('Got a response from the API');
+  }
 }

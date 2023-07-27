7f86c3315b9275481ad75163cdecaa688eb62fed
davidajimati <ajimatidavid@gmail.com>
2023-07-27 13:43:05 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => {
-      SpeechRecognitionResultList.map((result) => ({
+      result.map((result) => ({
         status: result.status,
         value: result.value || `Error: ${result.reason.message}`,
       }));

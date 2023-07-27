4cd9654c2e8e1b7abb771de6feb4daacea28c9f2
davidajimati <ajimatidavid@gmail.com>
2023-07-27 11:32:47 +0100

    2-then.js promises consumed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  try {
-    return new Promise((resolve, reject) => {
-      if (promise) {
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        reject(new Error());
-      }
-    });
-  } finally {
-    console.log('Got a response from the API');
-  }
+  return promise
+    .then(() => { status: 200, body: 'success' })
+    .catch(() => Error())
+    .finally('Got a response from the API')
 }

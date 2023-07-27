42c5aa35b3e49441885e0f49c99081b0077aafde
davidajimati <ajimatidavid@gmail.com>
2023-07-27 17:24:04 +0100

    queue variable returned

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,10 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     queue.push(mathFunction());
-  } catch (e) {
-    queue.push(`Error: ${e}`);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
   }
   finally {
     queue.push('Guardrail was processed');
   }
+  return queue;
 }

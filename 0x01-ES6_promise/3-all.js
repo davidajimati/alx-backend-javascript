2b3c6947dccc2034d9bcd43d760aa297a3938fce
davidajimati <ajimatidavid@gmail.com>
2023-07-27 12:38:23 +0100

    string literals used

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
-      console.log(data[1].body, data[0].firstName, data[0].lastName);
+      console.log(`${data[1].body} ${data[0].firstName} ${data[0].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');

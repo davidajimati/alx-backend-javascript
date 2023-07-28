e50d49cd8c881a24e7ce9c0144aef3436160f2c3
davidajimati <ajimatidavid@gmail.com>
2023-07-28 02:55:12 +0100

    if statment removed

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,8 +1,5 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof (name) != 'string' || typeof (code) != 'string') {
-      throw new Error('Arguments must be a number');
-    }
     this._name = name;
     this._code = code;
   }

8ff5e5335efadf9da053129c28ad74df215823f9
davidajimati <ajimatidavid@gmail.com>
2023-08-02 16:30:17 +0100

    8-clean_set.js exported, 9-groceries_list.js created and tested

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, start) {
+  if (start === '') return '';
   return [...set]
     .filter((item) => item.startsWith(start))
     .map((item) => (item ? item.slice(start.length) : ''))

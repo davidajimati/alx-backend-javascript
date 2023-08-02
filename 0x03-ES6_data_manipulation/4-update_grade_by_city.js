8757c1fc7543e4995803a50051f88cfdf255718a
davidajimati <ajimatidavid@gmail.com>
2023-08-02 13:36:25 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,12 @@
-export default function updateStudentGradeByCity() {
-
+export default function updateStudentGradeByCity(studentList, city, gradeList) {
+  const newStudents = studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const updatedGrade = gradeList.find((gradeItem) => gradeItem.id === student.id);
+      return {
+        ...student,
+        grade: updatedGrade ? updatedGrade.grade : 'N/A',
+      };
+    });
+  return newStudents;
 }

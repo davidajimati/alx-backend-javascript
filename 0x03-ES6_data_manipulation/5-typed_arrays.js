8e2a8f383b2fa99182787f6903650fa4a07b8a4f
davidajimati <ajimatidavid@gmail.com>
2023-08-02 15:39:18 +0100

    type checking removed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,4 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (typeof (length) !== 'number' || !typeof (position) !== 'number' || typeof (value) !== 'number') {
-    throw new Error('NAN');
-  }
   const int8 = new Int8Array(new ArrayBuffer(length));
   if (position < int8.length) {
     int8[position] = value;

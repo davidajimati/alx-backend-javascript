03a6db4d81ec4f37e997b6d5d9473d5611c6d010
davidajimati <ajimatidavid@gmail.com>
2023-08-02 15:35:55 +0100

    arguments typed checked, ArryBuffer and int8Array compressed to a line

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
+  if (typeof (length) !== 'number' || !typeof (position) !== 'number' || typeof (value) !== 'number') {
+    throw new Error('NAN');
+  }
+  const int8 = new Int8Array(new ArrayBuffer(length));
   if (position < int8.length) {
     int8[position] = value;
     return int8;

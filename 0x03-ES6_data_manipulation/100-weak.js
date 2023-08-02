720ab04b72296ccd0f86a80c3faf9d40fadf0cb5
davidajimati <ajimatidavid@gmail.com>
2023-08-02 18:12:00 +0100

    100-weak.js created

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,19 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  // Check if the endpoint exists in the weakMap
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    weakMap.set(endpoint, count + 1);
+  }
+
+  // Check if the number of queries is >= 5
+  const queryCount = weakMap.get(endpoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export default { weakMap, queryAPI };

437797e87131e682e3275c1e8301a6a06a09c922
davidajimati <ajimatidavid@gmail.com>
2023-08-03 12:42:46 +0100

    studentClass created

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -62,3 +62,20 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string): string {
     return (`${firstName.slice(0, 1)}. ${lastName}`)
 }
+class StudentClass {
+    _firstName: string;
+    _lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this._firstName;
+    }
+}
